9a085ef509617176664cd535473ef5ee9c29e1c0
Julian Mejia <julianricardomt@gmail.com>
2026-04-22 16:47:45 -0500

    Primer commit

---
+++ src/main/java/com/example/talleramadoymejia22deabaril/Talleramadoymejia22deabarilApplication.java
@@ -0,0 +1,13 @@
+package com.example.talleramadoymejia22deabaril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Talleramadoymejia22deabarilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Talleramadoymejia22deabarilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/talleramadoymejia22deabaril/Talleramadoymejia22deabarilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talleramadoymejia22deabaril;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Talleramadoymejia22deabarilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
